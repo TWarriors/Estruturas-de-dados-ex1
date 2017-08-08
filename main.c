4afaffa006a0d7113773a4b1d17afb539076e0bc
Marcos <Marcos@10.197.33.38>
2017-08-08 20:07:05 -0300

    Adição de atividades

--- main.c
+++ main.c
@@ -33,17 +33,29 @@ main() {
             case 2:
                 atv2();
             break;
+            case 3:
+                atv3();
+            break;
+            case 4:
+                atv4();
+            break;
         }
     }
 }
 atv3(){
-    double N1, N2, N3, R1, R2, R3;
+    int N1, N2;
+    double N3, R1=0, R2=0, R3=0;
     printf("\n Insira o primeiro numero\n");
-    scanf("%d",&N1);
-    printf("\n Insira o primeiro numero\n");
-    scanf("%d",&N2);
-    printf("\n Insira o primeiro numero\n");
-    scanf("%d",&N3);
-    
+    scanf("%i",&N1);
+    printf("\n Insira o segundo numero\n");
+    scanf("%i",&N2);
+    printf("\n Insira o terceiro numero, sendo ele real\n");
+    scanf("%lf",&N3);
+    R1 = ((N1*2)*(N2/2));
+    R2 = ((N1*3) + N3);
+    R3 = (N3*N3*N3);
+    printf("\n A:%lf B:%lf C:%lf",R1, R2, R3 );
 }
-
+atv4(){
+    int 
+}
