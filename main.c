7d28c73add078936834a5c43dc0256ce40291f8a
Marcos <Marcos@10.197.33.38>
2017-08-08 21:56:50 -0300

    Adição de atividades

--- main.c
+++ main.c
@@ -65,16 +65,43 @@ atv4(){
     //motores por maquina: 1=20cv 3=5cv 2=1cv
     int cm1 = 1500, cm2 = 600, cm3 = 300;
     int m1 = 1, m2 = 3, m3 = 2;
-    int qm1 ,qm2, qm3;
-    int ctm, qm;
+    int qm1 ,qm2, qm3, ctm;
+    int ctc, qm, ctp1, ctp2, ctp3;
     printf("\n Digite quantas maquinas serão usadas:");
-    printf("%i",&qm);
+    scanf("%i",&qm);
     qm1 = qm*m1;
     qm2 = qm*m2;
     qm3 = qm*m3;
     printf("\n Motor 1:%i \n Motor 2:%i \n Motor 3:%i",qm1, qm2, qm3);
-    ctm = (((qm*qm1)*cm1)+((qm*qm2)*cm2)+((qm*qm3)*cm3));
-        
-    
-    
+    ctc = (((qm*qm1)*cm1)+((qm*qm2)*cm2)+((qm*qm3)*cm3));
+    ctp1 = qm1*cm1;
+    ctp2 = qm2*cm2;
+    ctp3 = qm3*cm3;
+    ctm = (cm1+(m2*cm2)+(m3*cm3));
+    printf("\n Custo parcial do motor:%i \n Custo total da compra:%i \n Custo por tipo de motor: 20CV:%i \n 5CV:%i \n 1CV:%i \n", ctm, ctc, ctp1, ctp2, ctp3); 
 }
+atv7(){
+    int nm1, nm2, nm3, nmm, nmn;
+    printf("\n Digite o primeiro numero\n");
+    scanf("%i"&nm1);
+    printf("\n Digite o segundo numero\n");
+    scanf("%i"&nm2);
+    printf("\n Digite o terceiro numero\n");
+    scanf("%i"&nm3);
+    if(){
+        nm1 > nm2 >nm3;
+        nmm = nm1;
+        nmn = nm3
+    }
+        else if{
+            nm2 > nm1 > nm3;
+            nmm = nm2;
+            nmn = nm3;
+        }
+        else if{
+            nm3 > nm2 > nm1;
+            nmm = nm3;
+            nmn = nm1
+        }
+}
+
